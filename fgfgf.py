c78851cc7f7057193579d69ae90478f71804ac62
Name <email@example.ru>
2023-12-29 22:49:02 +0500

    8888

---
+++ fgfgf.py
@@ -0,0 +1,6 @@
+board = [[' ' for _ in range(3)] for _ in range(3)]
+def print_board(board):
+    for row in board:
+        print("-" * 9)
+print_board(5)
+
--- krs.py
+++ krs.py
@@ -1,12 +1,12 @@
 field=[[None,None,None],[None,None,None],[None,None,None]]
 #Ввод данных
-player='X'
-step=0
-while step<=8  :
+player="X"
+Game=True
+while Game :
  print(*field,sep='\n')
  if player=='X':
   strk=int(input('Игрок '+ player +' введите номер строки (1-3)\n'))
-  if 3>=strk>=1:
+  if 3>=strk>=1 :
    print('Выбрана строка '+ str(strk))
   else:
    print('Число за пределами игрового поля')
@@ -14,13 +14,16 @@ while step<=8  :
   if 3>=stlb>=1:
     print('Выбрана строка '+ str(strk)+ '\n''Выбран столбик ' +str(stlb))
     field[strk-1][stlb-1]='X'
-    print('Переход хода')
+    print('Переход хода\n')
     player="O"
     print(*field,sep='\n')
-    step=step+1
-    print(step)
   else:
     print('Число за пределами игрового поля')
+
+ if field[0][0]==player and field[1][0]==player and field[2][0]==player:
+   print('Игрок '+player+" Победил!")   
+   Game=False 
+   break
  if player=='O':
   strk=int(input('Игрок '+ player +' введите номер строки (1-3)\n'))
   if 3>=strk>=1:
@@ -32,12 +35,13 @@ while step<=8  :
   if 3>=stlb>=1:
     print('Выбрана строка '+ str(strk)+ '\n''Выбран столбик ' +str(stlb))
     field[strk-1][stlb-1]='O'
-    print('Переход хода')
+    print('Переход хода\n')
     player="X"
     print(*field,sep='\n')
-    step=step+1
-    print(step)
   else:
-    print('Число за пределами игрового поля')    
-else :
-  print('Game over')    
+    print('Число за пределами игрового поля') 
+       
+ if field[0][0]==player and field[1][0]==player and field[2][0]==player:
+  print('Игрок '+player+" Победил!")  
+  Game=False 
+  break
